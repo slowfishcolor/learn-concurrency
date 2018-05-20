d210154a0d5bc5fd3005cb7ce19589d57f709201
Prophet Xu <slowfishcolor@gmail.com>
2018-05-20 13:41:12 +0800

    first commit

---
+++ src/main/java/com/sfc/learnconcurrency/LearnConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.sfc.learnconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnConcurrencyApplication.class, args);
+    }
+}
